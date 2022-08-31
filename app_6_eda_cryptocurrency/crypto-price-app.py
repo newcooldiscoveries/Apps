3e562c6d66034e6efa535c813b0e4d7a611b796a
newcooldiscoveries <68378011+newcooldiscoveries@users.noreply.github.com>
2022-08-30 22:50:01 -0400

    Update crypto-price-app.py

--- app_6_eda_cryptocurrency/crypto-price-app.py
+++ app_6_eda_cryptocurrency/crypto-price-app.py
@@ -3,7 +3,8 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import base64
-import matplotlib.pyplot as plt
+import matplotlib
+import pyplot as plt
 from bs4 import BeautifulSoup
 import requests
 import json
